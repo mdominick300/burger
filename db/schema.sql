91ac0b73464ee60abe4c7a4e84dca40dd4c1a3c7
Matt Dominick <mdominick300@gmail.com>
2020-01-19 16:34:44 -0500

    first

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS burgers_DB;
+CREATE DATABASE burgers_DB;
+
+USE burgers_DB;
+
+CREATE TABLE burgers
+(
+  id INT NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(100) NOT NULL,
+  devoured BOOLEAN NOT NULL,
+  PRIMARY KEY (id)
+);
+
---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+USE burgers_DB;
+
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES (1,"Large", 1), (2,"Medium", 0),(3,"Small", 1);
