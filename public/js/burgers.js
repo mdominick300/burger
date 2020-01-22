d3dc8d2795aee93b4145453b210acd8e31deeb7a
Matt Dominick <mdominick300@gmail.com>
2020-01-21 21:23:11 -0500

    devourworks

--- public/js/burgers.js
+++ public/js/burgers.js
@@ -2,10 +2,10 @@
 $(function() {
     $(".change-devour").on("click", function(event) {
       var id = $(this).data("id");
-      var newDevour = $(this).data("newDevour");
+      var newDevour = $(this).data("newdevour");
   
       var newDevourState = {
-        devour: newDevour
+        devoured: newDevour
       };
   
     //   Send the PUT request.
